8bcbbe0d2f1df6ee96747b314d03eaa3365682c5
hosseinrezaeian <hossainrezaeian@gmail.com>
2023-11-09 00:24:59 +0330

    add change lang

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # translate_app
 this is a simple translate applcation python
+
+
+
+
+pyinstaller --onefile --name translate translate_app.py
--- translate_app.py
+++ translate_app.py
@@ -11,6 +11,7 @@ translator = Translator()
 # Create a subclass of QWidget for your application
 class TranslateApp(QWidget):
     entered_text=''
+    lang="fa"
     def __init__(self):
         super().__init__()
 
@@ -19,21 +20,26 @@ class TranslateApp(QWidget):
     def init_ui(self):
         self.text_field = QLineEdit()
         self.fortranslate = QLabel()
-
+        self.change_languge = QLabel()
         self.result_label = QLabel()
 
         submit_button = QPushButton('translate')
+        change_languge = QPushButton('change languege')
         listening_button=QPushButton('Listening')
         submit_button.clicked.connect(self.show_text)
+        change_languge.clicked.connect(self.ChangeLang)
         listening_button.clicked.connect(self.speack_text)
+        
         self.text_field.returnPressed.connect(submit_button.click)
 
-
-
+        
+        self.change_languge.setText(self.lang)
 
         layout = QVBoxLayout()
+        layout.addWidget(self.change_languge)
         layout.addWidget(self.text_field)
         layout.addWidget(submit_button)
+        layout.addWidget(change_languge)
         layout.addWidget(listening_button)
         layout.addWidget(self.fortranslate)
         
@@ -53,7 +59,7 @@ class TranslateApp(QWidget):
         self.setGeometry(100, 100, 400, 200)
         # layout.addWidget(self.checkbox) 
 
-
+    
 
 
 
@@ -69,30 +75,40 @@ class TranslateApp(QWidget):
         
     def show_text(self):
         self.entered_text = self.text_field.text()
-        self.text_field.clear()
+        self.text_field.setFocus()
+        
         if self.entered_text !="":
             self.fortranslate.setText(self.entered_text)
-            translation = translator.translate(self.entered_text, dest="fa")
+            translation = translator.translate(self.entered_text, dest="en" if self.lang=="fa" else "fa")
             self.result_label.setText(translation.text)
             
+    def ChangeLang(self):
+        self.lang="en" if self.lang=="fa" else "fa"
+        self.change_languge.setText(self.lang)
+
+
+
 
     def speack_text(self):
         if self.entered_text !="":
-            tts = gTTS(self.entered_text)
-            audio_bytes = io.BytesIO()
-            tts.write_to_fp(audio_bytes)
-            audio_bytes.seek(0)
+            try:
+                tts = gTTS(self.entered_text)
+                audio_bytes = io.BytesIO()
+                tts.write_to_fp(audio_bytes)
+                audio_bytes.seek(0)
 
-            # Initialize pygame mixer
-            pygame.mixer.init()
+                # Initialize pygame mixer
+                pygame.mixer.init()
 
-            # Load and play the audio from the bytes-like object
-            pygame.mixer.music.load(audio_bytes)
-            pygame.mixer.music.play()
+                # Load and play the audio from the bytes-like object
+                pygame.mixer.music.load(audio_bytes)
+                pygame.mixer.music.play()
 
-            # Wait for the audio to finish playing
-            while pygame.mixer.music.get_busy():
-                pygame.time.Clock().tick(10)
+                # Wait for the audio to finish playing
+                while pygame.mixer.music.get_busy():
+                    pygame.time.Clock().tick(10)
+            except:
+                "not ok"
 
 
 def main():
